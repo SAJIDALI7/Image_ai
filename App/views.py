4c8db7d996695c843135a4e4048c6022ac6952ed
SAJIDALI7 <sajidalicharawala773@gmail.com>
2025-02-27 15:31:44 +0530

    second commit

--- App/views.py
+++ App/views.py
@@ -9,7 +9,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from .models import GeneratedImage
 from .serializers import GeneratedImageSerializer, ImageGenerationSerializer
 
